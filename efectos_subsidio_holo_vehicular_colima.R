2050e6dd8cd690a0df62b5edd612899cee7185f4
Zatara13 <71557830+Zatara13@users.noreply.github.com>
2021-01-16 13:25:09 -0600

    Update efectos_subsidio_holo_vehicular_colima.R

--- efectos_subsidio_holo_vehicular_colima.R
+++ efectos_subsidio_holo_vehicular_colima.R
@@ -1,36 +1,36 @@
-## Efectos de subsidiar la calcomanÌa vehicular en Colima
+## Efectos de subsidiar la calcoman√≠a vehicular en Colima
 
-## øA quiÈn beneficiarÌa dejar de cobrar el holograma vehicular?
-## Supongamos que sigue una lÛgica de subsidio
-## Como es un impuesto, ahora todos debemos cubrir lo que ya no est·n aportando especificamente los hogares con un auto o m·s
-## Por cuestiones de simplicidad, vamos a asumir que solamente se subsidiar· un holograma vehicular por hogar, si los hogares tienen m·s de un vehÌculo
+## ¬øA qui√©n beneficiar√≠a dejar de cobrar el holograma vehicular?
+## Supongamos que sigue una l√≥gica de subsidio
+## Como es un impuesto, ahora todos debemos cubrir lo que ya no est√°n aportando especificamente los hogares con un auto o m√°s
+## Por cuestiones de simplicidad, vamos a asumir que solamente se subsidiar√° un holograma vehicular por hogar, si los hogares tienen m√°s de un veh√≠culo
 ## deberan cubrir el holograma del excedente
 
 ## Cargamos librerias de trabajo
 library(tidyverse) ## Hacer la tidymagia        
 library(readr) ## Leer la base de datos
-library(wesanderson) ## Los colores est·n geniales
+library(wesanderson) ## Los colores est√°n geniales
 
 ## Cargamos bases de datos
-viviendas_int_15 <- read_csv("https://raw.githubusercontent.com/Zatara13/sub_holo_vehi_col/main/TR_VIVIENDA06.CSV?token=ARB6FRQH67PRFU6TSJIW653AAIGDM")
-## Seleccionamos las variables de Sexo de Jefe del Hogar, Ingreso de Trabajo, Tenencia de automovil en el hogar y el factor de expansiÛn
+viviendas_int_15 <- read_csv("https://raw.githubusercontent.com/Zatara13/sub_holo_vehi_col/main/TR_VIVIENDA06.CSV")
+## Seleccionamos las variables de Sexo de Jefe del Hogar, Ingreso de Trabajo, Tenencia de automovil en el hogar y el factor de expansi√≥n
 viv_15 <- viviendas_int_15 %>%
       select(JEFE_SEXO, INGTRHOG, AUTOPROP, FACTOR)
 
-## Creamos base de datos para trabajar el an·lisis
+## Creamos base de datos para trabajar el an√°lisis
 viv_15_s <- viv_15 %>%
       drop_na() %>% ## Se eliminan las observaciones donde no hay datos en ingreso, sexo de jefe de familia o en tenecia de auto
       filter(AUTOPROP < 9) %>% ## Este filtro se hace para solo seleccionar los hogares donde hay certeza que hay o no hay auto
-      mutate(auto = if_else(AUTOPROP == 7, 1, 0)) %>% ## Esta transformaciÛn permite codificar tener auto como variable dicotÛmica
-      rename(jefe_sexo = JEFE_SEXO, ingtrhog = INGTRHOG, factor = FACTOR) %>% ## Renombramos a min˙sculas para trabajar con m·s comodidad
-      select(jefe_sexo, ingtrhog, auto, factor)  %>% ## seleccionamos variables de interÈs
-      filter(ingtrhog < 999999) %>% ## filtramos ingreso < 999999, porque ese es el cÛdigo de no se sabe
-      mutate(jefe_sexo = if_else(jefe_sexo == 1, "hombre", "mujer")) ## Transformamos de los cÛdigos a los nombres de categorÌa
+      mutate(auto = if_else(AUTOPROP == 7, 1, 0)) %>% ## Esta transformaci√≥n permite codificar tener auto como variable dicot√≥mica
+      rename(jefe_sexo = JEFE_SEXO, ingtrhog = INGTRHOG, factor = FACTOR) %>% ## Renombramos a min√∫sculas para trabajar con m√°s comodidad
+      select(jefe_sexo, ingtrhog, auto, factor)  %>% ## seleccionamos variables de inter√©s
+      filter(ingtrhog < 999999) %>% ## filtramos ingreso < 999999, porque ese es el c√≥digo de no se sabe
+      mutate(jefe_sexo = if_else(jefe_sexo == 1, "hombre", "mujer")) ## Transformamos de los c√≥digos a los nombres de categor√≠a
 
 ## Calculamos rangos de deciles con ingreso de Colima
 viv_15_ing <- viv_15 %>%
       drop_na(INGTRHOG) %>% ## Eliminamos las observaciones donde no hay datos en ingreso
-      filter(INGTRHOG < 999999) ## 999,999 es el cÛdigo para no especificÛ, por lo que seleccionamos las respuestas menores a esta
+      filter(INGTRHOG < 999999) ## 999,999 es el c√≥digo para no especific√≥, por lo que seleccionamos las respuestas menores a esta
 quantile(viv_15_ing$INGTRHOG,  prob = seq(0, 1, length = 11), type = 5)
 rm(viviendas_int_15, viv_15_ing)
 
@@ -60,14 +60,14 @@ dec_7 <- fing_md(9001, 10800, 7)
 dec_8 <- fing_md(10801, 14000, 8)
 # IX [14001, 19714]
 dec_9 <- fing_md(14001, 19714, 9)
-# X [19715, 999998] # El 999998 tiene que ver con la codificaciÛn del ingreso en la encuesta intercensal, que implica que ganan mucha lana
+# X [19715, 999998] # El 999998 tiene que ver con la codificaci√≥n del ingreso en la encuesta intercensal, que implica que ganan mucha lana
 dec_10 <- fing_md(19715, 999998, 10)
 
-ingmed <-  bind_rows(dec_1, dec_2,dec_3,dec_4,dec_5,dec_6,dec_7,dec_8,dec_9,dec_10) ## AquÌ podemos ver el ingreso medio por decil
+ingmed <-  bind_rows(dec_1, dec_2,dec_3,dec_4,dec_5,dec_6,dec_7,dec_8,dec_9,dec_10) ## Aqu√≠ podemos ver el ingreso medio por decil
 rm(dec_1, dec_2,dec_3,dec_4,dec_5,dec_6,dec_7,dec_8,dec_9,dec_10, fing_md)
 
-## Con esta funciÛn calculamos el costo promedio de subsidiar por decil
-## Usamos la fÛrmula Costo del holograma vehicular * probabilidad de tener carro en el decil * suma de factores de expansiÛn del decil
+## Con esta funci√≥n calculamos el costo promedio de subsidiar por decil
+## Usamos la f√≥rmula Costo del holograma vehicular * probabilidad de tener carro en el decil * suma de factores de expansi√≥n del decil
 fhog_ing <- function(ingmin, ingmax, nombre_decil){
       viv_15_s %>%
             group_by(jefe_sexo) %>% ## agrupamos por variable de sexo para ver efecto diferenciado en hogares con jefe de familia o con jefa de familia
@@ -76,7 +76,7 @@ fhog_ing <- function(ingmin, ingmax, nombre_decil){
             summarise(p_auto = sum(p), hogares = sum(factor)) %>% ##Calculamos la probabilidad por decil de que un hogar tenga auto
             mutate(subsidio_mdp = p_auto * hogares * 1043 / 1000000) %>% ## Dividimos entre 1,000,000 para tener el subsidio por millones de pesos
             mutate(decil = nombre_decil) %>% ## Agregamos codificador de decil
-            mutate(hog_auto = p_auto * hogares) ## Nos permite saber el n˙mero de hogares con auto por decil
+            mutate(hog_auto = p_auto * hogares) ## Nos permite saber el n√∫mero de hogares con auto por decil
 }
 
 ## Deciles
@@ -98,18 +98,18 @@ dec_7 <- fhog_ing(9001, 10857, 7)
 dec_8 <- fhog_ing(10858, 14000, 8)
 # IX [14001, 19714]
 dec_9 <- fhog_ing(14001, 19714, 9)
-# X [19715, 999998] # El 999998 tiene que ver con la codificaciÛn del ingreso en la encuesta intercensal, que implica que ganan mucha lana
+# X [19715, 999998] # El 999998 tiene que ver con la codificaci√≥n del ingreso en la encuesta intercensal, que implica que ganan mucha lana
 dec_10 <- fhog_ing(19715, 999998, 10)
 
-## Unimos los datos de los deciles, y agregamos la distribuciÛn del subsidio por cada 100 pesos (aka porcentaje)
+## Unimos los datos de los deciles, y agregamos la distribuci√≥n del subsidio por cada 100 pesos (aka porcentaje)
 sub_decil_au <- bind_rows(dec_1, dec_2,dec_3,dec_4,dec_5,dec_6,dec_7,dec_8,dec_9,dec_10) %>% ## Unimos los objetos creados previamente
-      select(jefe_sexo, decil, subsidio_mdp, hogares, hog_auto) %>% ## Seleccionamos variable de interÈs
-      mutate(pct_subs = subsidio_mdp / sum(subsidio_mdp) * 100) ## Calculamos distribuciÛn porcentual del apoyo
+      select(jefe_sexo, decil, subsidio_mdp, hogares, hog_auto) %>% ## Seleccionamos variable de inter√©s
+      mutate(pct_subs = subsidio_mdp / sum(subsidio_mdp) * 100) ## Calculamos distribuci√≥n porcentual del apoyo
 rm(dec_1, dec_2,dec_3,dec_4,dec_5,dec_6,dec_7,dec_8,dec_9,dec_10)
 
 sub_decil_au %>% summarise(sum(subsidio_mdp * 1000000)) ## Vemos la suma del subsidio
-## Comprobamos que operaciÛn sirva. 
-viv_15_s %>% summarise(sum(auto * factor * 1043)) == sub_decil_au %>% summarise(sum(subsidio_mdp * 1000000)) ## Si resulta TRUE, no hay errores de cÛdigo
+## Comprobamos que operaci√≥n sirva. 
+viv_15_s %>% summarise(sum(auto * factor * 1043)) == sub_decil_au %>% summarise(sum(subsidio_mdp * 1000000)) ## Si resulta TRUE, no hay errores de c√≥digo
 
 ## Graficas
 
@@ -117,7 +117,7 @@ viv_15_s %>% summarise(sum(auto * factor * 1043)) == sub_decil_au %>% summarise(
 ggplot(sub_decil_au, aes(x = decil, y = subsidio_mdp, fill = jefe_sexo)) +
       theme_bw()+
       geom_bar(stat="identity") +
-      geom_vline(xintercept = 1.5, ## Seleccionamos 1.5 para representar la lÌnea de pobreza urbana (CA + CNA) de dic-15
+      geom_vline(xintercept = 1.5, ## Seleccionamos 1.5 para representar la l√≠nea de pobreza urbana (CA + CNA) de dic-15
                  linetype = "dashed", color = "red")+
       scale_fill_manual(values=wes_palette(n=2, name="GrandBudapest2"))+
       scale_x_continuous(breaks = c(1:10), labels = c("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"))+
@@ -126,13 +126,13 @@ ggplot(sub_decil_au, aes(x = decil, y = subsidio_mdp, fill = jefe_sexo)) +
            x = "Decil de ingresos",
            y = "Subsidio en millones pesos",
            caption = "Fuente: Encuesta Intercensal 2015, INEGI
-           Nota: A la derecha de la lÌnea roja est·n los deciles con ingresos medios superiores a la lÌnea de pobreza  urbana (CA + CNA) de diciembre de 2015")
+           Nota: A la derecha de la l√≠nea roja est√°n los deciles con ingresos medios superiores a la l√≠nea de pobreza  urbana (CA + CNA) de diciembre de 2015")
 
 ## Destino de cada 100 pesos del subsidio, por decil
 ggplot(sub_decil_au, aes(x = decil, y = pct_subs, fill = jefe_sexo)) +
       theme_bw()+
       geom_bar(stat="identity") +
-      geom_vline(xintercept = 1.5, ## Seleccionamos 1.5 para representar la lÌnea de pobreza urbana (CA + CNA) de dic-15
+      geom_vline(xintercept = 1.5, ## Seleccionamos 1.5 para representar la l√≠nea de pobreza urbana (CA + CNA) de dic-15
                  linetype = "dashed", color = "red")+
       scale_fill_manual(values=wes_palette(n=2, name="GrandBudapest2"))+
       scale_x_continuous(breaks = c(1:10), labels = c("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"))+
@@ -141,4 +141,4 @@ ggplot(sub_decil_au, aes(x = decil, y = pct_subs, fill = jefe_sexo)) +
            x = "Decil de ingresos",
            y = "Pesos",
            caption = "Fuente: Encuesta Intercensal 2015, INEGI
-           Nota: A la derecha de la lÌnea roja est·n los deciles con ingresos medios superiores a la lÌnea de pobreza  urbana (CA + CNA) de diciembre de 2015")
+           Nota: A la derecha de la l√≠nea roja est√°n los deciles con ingresos medios superiores a la l√≠nea de pobreza  urbana (CA + CNA) de diciembre de 2015")
